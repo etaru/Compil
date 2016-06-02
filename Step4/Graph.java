9238ea8a6411a4b72ab35e6780dd32a3cb5bd49c
etaru <taruerick@gmail.com>
2016-06-03 00:19:24 +0200

    getDegre

--- Step4/Graph.java
+++ Step4/Graph.java
@@ -2,19 +2,30 @@
 
 public class Graph {
 
-	ArrayList<String> = sommet;
-	ArrayList<String> = interference;
-	ArrayList<String> = preference;
+	ArrayList<String> = sommets; //liste des sommets
+	ArrayList<String> = interferences; //liste d'aretes d'interferences (relie 2 sommets)
+	ArrayList<String> = preference; //liste d'aretes de preferences (relie 2 sommets)
+	int nbCouleur; //nombre de couleur
+	ArrayList<String> = couleurs; //liste des couleurs des sommets
 
-	Graph(ArrayList<String> s, ArrayList<String> interference, ArrayList<String> preference) {
-		this.sommet = s;
-		this.interference = i;
-		this.preference = p;
+	Graph(ArrayList<String> s, ArrayList<String> i, ArrayList<String> p) {
+		this.sommets = s;
+		this.interferences = i;
+		this.preferences = p;
 	}
 
 	//fonction pour avoir le degre d'un sommet du graphe
 	int getDegre(String sommet) {
 		int degre = 0;
-		//a completer
 		
+		for(int i=0;i<interferences.size();i++) { //parcourt toutes les interferences
+			String [] extremite = interference.get(i).split(","); //recupere les extremites de l'arete
+			
+			if (extremite[0]==sommet || extremite[1]==sommet) { //si une extremites correspond au sommet mis en parametre, on incremente le degre
+				degre = degre+1;
+			}
+		}
+		return degre;
 	}
+	
+	
