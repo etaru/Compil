de8ef4b0475fea36d16f63dd9be70df96795f272
etaru <taruerick@gmail.com>
2016-06-02 12:49:09 +0200

    Step4

---
+++ Step4/Graph.java
@@ -0,0 +1,16 @@
+/*Coloring Graph*/
+
+public class Graph {
+
+	ArrayList<String> = sommet;
+	ArrayList<String> = interf;
+	ArrayList<String> = pref;
+
+	Graph(ArrayList<String> s, ArrayList<String> interf, ArrayList<String> pref) {
+		this.sommet = s;
+		this.interf = i;
+		this.pref = p;
+	}
+
+	
+}
